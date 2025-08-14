8a54b41d1780f47d917bf5fc7644195a67f472ee
estr05 <estradaperezneyser@gmail.com>
2025-08-13 23:12:17 -0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// config.php
+
+// Configuraciones de la base de datos
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "arrendaoco"); // Asegúrate de usar el nombre correcto de la base de datos
+?>
